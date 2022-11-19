ce2494403dc4884728a83a66e0bbcf0ca8c16f58
DevIdol <devidol.mm@gmail.com>
2022-11-19 18:05:48 +0630

    Modified Design on 19 Nov 2022

--- js/common.js
+++ js/common.js
@@ -1,17 +1,26 @@
 $(document).ready(function () {
-  $(".menu-toggle").on("click", function () {
-    var topVal = 0;
-    if ($(this).hasClass("active")) {
-      topVal = -980;
-      $(this).removeClass("active");
-    } else {
-      $(this).addClass("active");
-    }
-    $(".g-nav").stop().animate(
-      {
-        top: topVal,
-      },
-      400
-    );
-  });
+    $(".menu-toggle").on("click", function () {
+        var topVal = 0;
+        if ($(this).hasClass("active")) {
+            topVal = -980;
+            $(this).removeClass("active");
+        } else {
+            $(this).addClass("active");
+        }
+        $(".g-nav").stop().animate(
+            {
+                top: topVal,
+            },
+            400
+        );
+        $(".g-nav a").click(function () {
+            $(".menu-toggle").removeClass("active");
+            $(".g-nav").stop().animate(
+                {
+                    top: -980,
+                },
+                400
+            );
+        });
+    });
 });
